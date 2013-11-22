135df0c26312683b701a38b06bd1976f6de05650
damonshen <thanatos1710@hotmail.com>
2013-11-22 22:59:34 +0800

    create the java project for parsing the open source data

---
+++ ParseOpenData/src/dataParser.java
@@ -0,0 +1,17 @@
+/*
+ * This code is used for parse the data from 
+ * http://data.gov.tw/opendata/Details?sno=345000000G-00014 and
+ * http://data.gov.tw/opendata/Details?sno=355000000I-00005.
+ * 
+ * */
+public class dataParser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
