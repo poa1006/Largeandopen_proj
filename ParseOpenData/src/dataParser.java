aaf385339556533cdf2de4187326799a15433c46
iGaryGu <gary640201a@gmail.com>
2013-11-27 03:39:57 +0800

    get data

--- ParseOpenData/src/dataParser.java
+++ ParseOpenData/src/dataParser.java
@@ -8,9 +8,12 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class dataParser {
@@ -24,11 +27,50 @@ public class dataParser {
 	
 	public static void main(String[] args) throws IOException, JSONException {
 		// TODO Auto-generated method stub
-		String farmData = readFileToString("../farm.txt");		
+		String farmData = readFileToString("../farm.txt");	
+		String riverData = readFileToString("../river.txt");
 		JSONArray farmArray = new JSONArray(farmData); 
+		JSONArray riverArray = new JSONArray(riverData);
+		parsefarm(farmArray);
+		parseriver(riverArray);
 		
 	}
-
+	
+	//extract information from the farm.txt
+	public static void parsefarm(JSONArray farmarray) throws JSONException{
+		for(int i = 0 ; i < farmarray.length();i++){
+			JSONObject object = farmarray.getJSONObject(i);
+			String farm_name = object.get("Farm_name").toString();
+			String city = object.get("city").toString();
+			String area = object.get("area").toString();
+			
+			System.out.println(i +" "+farm_name+"+"+city+"+"+area+"|");
+			
+		}
+	}
+	
+	//extract information from river.txt
+	//itemname : pH/Suspended Solid/Dissolved Oxygen/River Pollution Index/NH3-N/Chemical Oxygen Demand/Chloride
+	//itemvalue : there are serveral data without value and some data show "<0.01" ,"<0.1"
+	
+	public static void parseriver(JSONArray riverarray) throws JSONException{
+		for(int i = 0 ; i < riverarray.length();i++){
+			JSONObject object = riverarray.getJSONObject(i);
+			String basin = object.get("Basin").toString();
+			String longitude = object.get("TWD97Lon").toString();
+			String latitude = object.get("TWD97Lat").toString();
+			String itemname = object.get("ItemEngName").toString();
+			String itemvalue = object.get("ItemValue").toString();
+			String itemunit =  object.get("ItemUnit").toString();
+			
+			
+			System.out.println(i +" "+basin+"+"+longitude+"+"+latitude+"+"+itemname+"+"+itemvalue+"+"+itemunit+"|");
+			
+		}
+	}
+	
+	
+	
 	public static String readFileToString(String filePath) throws IOException{
 		StringBuilder fileData = new StringBuilder(1000);
 		BufferedReader reader = new BufferedReader(new FileReader(filePath));
@@ -42,6 +84,5 @@ public class dataParser {
 		reader.close();
 		return  fileData.toString();	
 	}
-	
 	
 }
