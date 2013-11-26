1e276bae7f2941d552b91511f694494620307c62
damonshen <thanatos1710@hotmail.com>
2013-11-26 22:58:58 +0800

    add json.jar into project and parse the infomation of the farms.

--- ParseOpenData/src/dataParser.java
+++ ParseOpenData/src/dataParser.java
@@ -3,15 +3,45 @@
  * http://data.gov.tw/opendata/Details?sno=345000000G-00014 and
  * http://data.gov.tw/opendata/Details?sno=355000000I-00005.
  * 
+ * 
  * */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+
 public class dataParser {
 
 	/**
 	 * @param args
+	 * @throws IOException 
+	 * @throws JSONException 
 	 */
-	public static void main(String[] args) {
+	
+	
+	public static void main(String[] args) throws IOException, JSONException {
 		// TODO Auto-generated method stub
-
+		String farmData = readFileToString("../farm.txt");		
+		JSONArray farmArray = new JSONArray(farmData); 
+		
 	}
 
+	public static String readFileToString(String filePath) throws IOException{
+		StringBuilder fileData = new StringBuilder(1000);
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+		char[] buf = new char[10];
+		int numRead = 0;
+		while ((numRead = reader.read(buf)) != -1) {
+			String readData = String.valueOf(buf, 0, numRead);
+			fileData.append(readData);
+			buf = new char[1024];
+		}
+		reader.close();
+		return  fileData.toString();	
+	}
+	
+	
 }
